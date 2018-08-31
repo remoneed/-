58cab6ac141fa33939251ddf74a4805e99fb9c8e
poni0626 <42823131+poni0626@users.noreply.github.com>
2018-08-31 20:48:04 +0900

    unity

---
+++ tyatto_apr -/Assets/Ip_text.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Ip_text : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Text>().text = "IP" + UnityEngine.Network.player.ipAddress;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ tyatto_apr -/Assets/tyatt.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+public class tyatt : NetworkBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        //自分以外であればreturn
+        if (!isLocalPlayer)
+            return;
+        //PostingButtonを押したときにPostingメソッドが呼ばれるようにする
+        GameObject.Find("Button").GetComponent<Button>().onClick.AddListener(() => Posting());
+    }
+    public void Posting()
+    {
+        InputField inputField = GameObject.Find("InputField").GetComponent<InputField>();
+        //クライアント側からサーバにあるPlayerクラスのClone内のメソッドを呼ぶ
+        CmdPosting(inputField.text.ToString());
+        //inputFieldを白紙にする
+        inputField.text = "";
+    }
+    [Command]
+    public void CmdPosting(string ms)
+    {
+        RpcPosting(ms);
+    }
+    [ClientRpc]
+    public void RpcPosting(string ms)
+    {
+        Text chatText = GameObject.Find("chatText").GetComponent<Text>();
+        chatText.text = ms + "\n" + chatText.text;
+    }
+
+}
